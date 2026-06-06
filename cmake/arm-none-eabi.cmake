8738c3a52b0b62c6457db3c2fb6e1e6ad7505d08
estanislaocrivos <estanislaocrivos@gmail.com>
2026-06-06 15:23:54 -0300

    chore(cmake): simplify toolchain configuration and improve error handling

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -4,45 +4,24 @@
 set(CMAKE_SYSTEM_NAME Generic)        # "Generic" = bare metal (no host OS)
 set(CMAKE_SYSTEM_PROCESSOR arm)       # Target architecture
 
-# Common installation paths for ARM GCC toolchain
-set(ARM_TOOLCHAIN_HINTS
-    # macOS - ARM Developer website installation
-    "/Applications/ArmGNUToolchain/15.2.rel1/arm-none-eabi/bin"
-    "/Applications/ArmGNUToolchain/14.2.rel1/arm-none-eabi/bin"
-    "/Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/bin"
-    # macOS - Homebrew
-    "/opt/homebrew/bin"
-    "/usr/local/bin"
-    # Linux common paths
-    "/usr/bin"
-    "/usr/local/gcc-arm-none-eabi/bin"
-    "$ENV{HOME}/.local/bin"
-)
-
-# Allow override via environment variable
-if(DEFINED ENV{ARM_TOOLCHAIN_PATH})
-    list(INSERT ARM_TOOLCHAIN_HINTS 0 "$ENV{ARM_TOOLCHAIN_PATH}/bin")
+# Verify ARM toolchain is available in PATH before CMake tries to use it
+find_program(ARM_GCC arm-none-eabi-gcc)
+if(NOT ARM_GCC)
+    message(FATAL_ERROR
+        "arm-none-eabi-gcc not found in PATH.\n"
+        "Install the ARM GNU Toolchain and add its 'bin/' directory to your PATH:\n"
+        "  https://developer.arm.com/downloads/-/arm-gnu-toolchain-downloads")
 endif()
 
-# Find ARM GCC compiler
-find_program(ARM_GCC
-    NAMES arm-none-eabi-gcc
-    HINTS ${ARM_TOOLCHAIN_HINTS}
-    REQUIRED
-)
-get_filename_component(ARM_TOOLCHAIN_BIN ${ARM_GCC} DIRECTORY)
-
-message(STATUS "ARM toolchain found: ${ARM_TOOLCHAIN_BIN}")
-
 # Specify compilers
-set(CMAKE_C_COMPILER "${ARM_TOOLCHAIN_BIN}/arm-none-eabi-gcc")
-set(CMAKE_CXX_COMPILER "${ARM_TOOLCHAIN_BIN}/arm-none-eabi-g++")
-set(CMAKE_ASM_COMPILER "${ARM_TOOLCHAIN_BIN}/arm-none-eabi-gcc")
+set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
+set(CMAKE_ASM_COMPILER "arm-none-eabi-gcc")
 
 # Additional tools
-set(CMAKE_OBJCOPY "${ARM_TOOLCHAIN_BIN}/arm-none-eabi-objcopy")
-set(CMAKE_OBJDUMP "${ARM_TOOLCHAIN_BIN}/arm-none-eabi-objdump")
-set(CMAKE_SIZE "${ARM_TOOLCHAIN_BIN}/arm-none-eabi-size")
+set(CMAKE_OBJCOPY "arm-none-eabi-objcopy")
+set(CMAKE_OBJDUMP "arm-none-eabi-objdump")
+set(CMAKE_SIZE "arm-none-eabi-size")
 
 # Common flags for Cortex-M4 with FPU
 set(CPU_FLAGS "-mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard")
